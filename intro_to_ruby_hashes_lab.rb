ab013520081346c45a0e120b5c2e880dba32ce50
Mark Bihag <markbihag330@gmail.com>
2019-08-09 05:57:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,9 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  { key: value }
+  # { key: value }
+  hash = {}
+  hash[key] = value
 end
 
 def read_from_hash(hash, key)
